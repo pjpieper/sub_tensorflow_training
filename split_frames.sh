54114b30e20c342b45f4b92aa242137f3c97bbe4
CoryBorek <agentdid127cb@aol.com>
2022-04-15 20:50:18 -0400

    test

--- split_frames.sh
+++ split_frames.sh
@@ -7,3 +7,6 @@ $IMAGE_LENGTH = {$3/$2}
 mkdir ./model
 mkdir ./model/images
 ./sub_utilities/src/network_wrapper/training/scripts/split.sh $1 ./model/images $2 
+
+cd labelImg/
+python3 labelImg.py ../model/images/
