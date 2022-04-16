5d3f4a367470d064adf56f8cfef3c5e8f192ce04
CoryBorek <agentdid127cb@aol.com>
2022-04-15 23:05:31 -0400

    test

--- train.sh
+++ train.sh
@@ -13,7 +13,7 @@ cp ./train.config ./training/
 
 echo "Moving Training folder to setup training"
 cp -a ./training/ ./sub_utilities/src/network_wrapper/tf_tft_models/third_party/models/research/object_detection/legacy/
-cd ./sub_utilities/src/network_wrapper/tf_tft_models/third_party/models/research/object_detection/legacy/
+cd sub_utilities/src/network_wrapper/tf_tft_models/third_party/models/research/object_detection/legacy/
 
 echo "Beginning Training"
 python3 train.py --logtostderr --train_dir=training/ --pipeline_config_path=training/train.config
