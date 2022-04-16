be64311e73ebb053369493865938cfae10d3eed1
CoryBorek <agentdid127cb@aol.com>
2022-04-16 01:26:51 -0400

    test

--- split_frames.sh
+++ split_frames.sh
@@ -3,12 +3,12 @@
 #$1 = FILE, $2 = Video Length, $3 = Frames wanted
 echo "Splitting a video file into frames."
 
-$IMAGE_LENGTH = {$3/$2}
+IMAGE_LENGTH = {$3/$2}
 mkdir ./model
 mkdir ./model/images
 mkdir ./model/annotations
 
-./sub_utilities/src/network_wrapper/training/scripts/split.sh $1 ./model/images $2 
+./sub_utilities/src/network_wrapper/training/scripts/split.sh $1 ./model/images $IMAGE_LENGTH
 
 cd labelImg/
 python3 labelImg.py ../model/images/
