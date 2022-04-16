5bba508f19f440045a81788cea39defb8d79cb06
CoryBorek <agentdid127cb@aol.com>
2022-04-15 20:49:22 -0400

    made a few changes

--- install.sh
+++ install.sh
@@ -4,20 +4,23 @@
 echo "Installing Git and FFMPEG"
 sudo apt install git ffmpeg
 
+#Sub Utilities
 echo "Downloading Sub Utilities and installing Network Wrapper"
 git clone https://github.com/ksu-auv-team/sub_utilities/
 cd sub_utilities/
 git checkout network_wrapper_changes # Until it's merged into master
-
-
 cd src/network_wrapper/
-
 sh install.sh
 
-
+#labelImg
+echo "Done with Network Wrapper. Downloading and Installing labelImg."
 cd ../../..
 git clone https://github.com/tzutalin/labelImg.git
 cd labelImg
-pip3 install pyqt5 lxml
+sudo apt install pyqt5-dev-tools
+sudo pip3 install -r requirements/requirements-linux-python3.txt
 make qt5py3
+cd ../
 
+#Notify completion
+echo "Done!"
--- split_frames.sh
+++ split_frames.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#$1 = FILE, $2 = Video Length, $3 = Frames wanted
+echo "Splitting a video file into frames."
+
+$IMAGE_LENGTH = {$3/$2}
 mkdir ./model
 mkdir ./model/images
-./sub_utilities/src/network_wrapper/training/scripts/split.sh $1 $2 ./model/images
+./sub_utilities/src/network_wrapper/training/scripts/split.sh $1 ./model/images $2 
